24a08830b8c66f4714f76bde6f69b313fe371932
Simrit <kaulsimrit@gmail.com>
2024-10-02 13:05:29 +0530

    Added basic validations

--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,7 +1,13 @@
+import { IsNotEmpty } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class UpdateTaskDto {
+  @IsNotEmpty()
   title: string;
+
+  @IsNotEmpty()
   description: string;
+
+  @IsNotEmpty()
   status: TaskStatus;
 }
